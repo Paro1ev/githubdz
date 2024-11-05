870f3cdf0078d0ddcd2084da01188a3dd7570530
Паролев <kirill.loginov@mail.ru>
2024-11-06 00:36:01 +0500

    add class order, encapsulate and generate constructor

--- dz.cs
+++ dz.cs
@@ -4,3 +4,30 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+class order 
+{
+    int id;
+    string product;
+    string sender;
+    string consignee;
+    string address;
+    string status;
+
+public order(int id, string product, string sender, string consignee, string address, string status)
+        {
+            Id = id;
+            Product = product;
+            Sender = sender;
+            Consignee = consignee;
+            Address = address;
+            Status = status;
+        }    
+    public Int Id { get => id; set => id = value; }
+    public String Product { get => product; set => product = value; }
+    public String Sender { get => sender; set => sender = value; }
+    public String Consignee { get => consignee; set => consignee = value; }
+    public String Address { get => address; set => address = value; }
+    public String Status { get => status; set => status = value; }
+}
